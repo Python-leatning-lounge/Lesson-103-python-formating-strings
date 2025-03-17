919f19cd0b3e51ffaf7571d1640ff509fbf0989e
César Gabriel Morín Corona <121568700+Cesar-Gabriel@users.noreply.github.com>
2025-03-17 14:25:13 -0600

    Create lesson3.py

---
+++ lesson3.py
@@ -0,0 +1,9 @@
+# stings are just like variables byt you don't need to cut the code
+# to set one you'll do the same with the variables, example
+name = "Jhon Doe"
+age = 50
+# now printing is the same
+name = "Jhon Doe"
+age = 50
+print(f"My name is {name} and I am {age} years old.")
+# this lesson is shorter than i thougt
